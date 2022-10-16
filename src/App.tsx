12c03da4556504d3fdf1f40843443d5bc9b8b64b
Aziz <aziz.khadjayev070@gmail.com>
2022-10-16 19:55:10 +0200

    APP-04 Clean up index file

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import "./App.css";
-import Button from "@mui/material/Button";
 
 function App() {
   return (
     <>
-      <Button variant="contained">Hello World</Button>
-      <h1>We</h1>
+      <h1>Welcome</h1>
     </>
   );
 }
