e394f911efced2723923aa2626bff756849e7d94
Aziz <aziz.khadjayev070@gmail.com>
2022-10-18 21:11:03 +0200

    APP-04 Navbar add hidebar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,18 +1,36 @@
 import * as React from "react";
+import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
 import CssBaseline from "@mui/material/CssBaseline";
+import useScrollTrigger from "@mui/material/useScrollTrigger";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
+import Slide from "@mui/material/Slide";
 
 interface Props {
   window?: () => Window;
   children: any;
 }
 
+function HideOnScroll(props: Props) {
+  const { children, window } = props;
+  const trigger = useScrollTrigger({
+    target: window ? window() : undefined,
+  });
+
+  return (
+    <Slide appear={false} direction="down" in={!trigger}>
+      {children}
+    </Slide>
+  );
+}
+
 function Navbar(props: Props) {
   return (
     <React.Fragment>
       <CssBaseline />
+
       <Toolbar />
       <Container>
         <Box sx={{ my: 2 }}>
