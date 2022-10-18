67ddf9d9764bd0568913f9f0d7e30e5891d9b8cf
Aziz <aziz.khadjayev070@gmail.com>
2022-10-18 21:14:38 +0200

    APP-04 Use in app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <>
+      <Navbar children={undefined} />
       <h1>Welcome</h1>
     </>
   );
