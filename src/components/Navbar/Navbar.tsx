cf34d53f4824895234751b0cd8a1fefb1ae939e0
Aziz <aziz.khadjayev070@gmail.com>
2022-10-18 21:13:05 +0200

    APP-04 Remove content

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -40,18 +40,6 @@ function Navbar(props: Props) {
         </AppBar>
       </HideOnScroll>
       <Toolbar />
-      <Container>
-        <Box sx={{ my: 2 }}>
-          {[...new Array(72)]
-            .map(
-              () => `Cras mattis consectetur purus sit amet fermentum.
-Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`
-            )
-            .join("\n")}
-        </Box>
-      </Container>
     </React.Fragment>
   );
 }
