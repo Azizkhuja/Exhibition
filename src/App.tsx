325efce9743c39c0b54586d3585c7a38ffac5801
Aziz <aziz.khadjayev070@gmail.com>
2022-10-19 15:01:48 +0200

    APP-04 Use home component in app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
 
 function App() {
   return (
     <>
-      <Navbar children={undefined} />
-      <h1>Welcome</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </>
   );
 }
