f5e81e15cf9e7308267171197214dfb0411ba072
Aziz <aziz.khadjayev070@gmail.com>
2022-10-21 18:26:15 +0200

    APP-04 Change home file stracture

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Home from "./components/Home/Home";
+import Home from "./components/Home";
 
 function App() {
   return (
--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,4 +1,4 @@
-import Navbar from "../Navbar/Navbar";
+import Navbar from "./Navbar/Navbar";
 
 const Home = () => {
   return <Navbar children={undefined} />;
