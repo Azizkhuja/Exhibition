c2bec0b020b4b6c1e9b09c250295b271cb0e2e85
Aziz <aziz.khadjayev070@gmail.com>
2022-10-22 10:52:31 +0200

    APP-04 Import mui components

--- src/components/Home/Main/Main.tsx
+++ src/components/Home/Main/Main.tsx
@@ -1,3 +1,8 @@
+import Box from "@mui/material/Box";
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
+import ImageListItemBar from "@mui/material/ImageListItemBar";
+
 const Main = () => {
   return <div>Main</div>;
 };
