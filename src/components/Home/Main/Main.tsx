bbda1024c119b7aff07b4aabef76e4f118d19931
Aziz <aziz.khadjayev070@gmail.com>
2022-10-22 11:11:52 +0200

    APP-04 Wrap image with link

--- src/components/Home/Main/Main.tsx
+++ src/components/Home/Main/Main.tsx
@@ -1,21 +1,24 @@
 import { Box, ImageList, ImageListItem, ImageListItemBar } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const Main = () => {
   return (
     <Box>
-      <ImageList variant="woven" cols={3} gap={8}>
-        {itemData.map((item) => (
-          <ImageListItem key={item.img}>
-            <img
-              src={`${item.img}?w=248&fit=crop&auto=format`}
-              srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
-              alt={item.title}
-              loading="lazy"
-            />
-            <ImageListItemBar position="below" title={item.author} />
-          </ImageListItem>
-        ))}
-      </ImageList>
+      <Link to="/visit">
+        <ImageList variant="woven" cols={3} gap={8}>
+          {itemData.map((item) => (
+            <ImageListItem key={item.img}>
+              <img
+                src={`${item.img}?w=248&fit=crop&auto=format`}
+                srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                alt={item.title}
+                loading="lazy"
+              />
+              <ImageListItemBar position="below" title={item.author} />
+            </ImageListItem>
+          ))}
+        </ImageList>
+      </Link>
     </Box>
   );
 };
