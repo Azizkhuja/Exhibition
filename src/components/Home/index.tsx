61d7c07879994a811c2f9ca430799ee999497e20
Aziz <aziz.khadjayev070@gmail.com>
2022-10-25 09:44:08 +0200

    APP-04 Add divivder

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,11 +1,13 @@
 import Main from "./Main/Main";
 import Navbar from "./Navbar/Navbar";
+import Divider from "@mui/material/Divider";
 
 const Home = () => {
   return (
     <>
       <Navbar children={undefined} />
       <Main />
+      <Divider />
     </>
   );
 };
