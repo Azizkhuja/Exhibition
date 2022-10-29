5eb76ee2fab57354a048a56ef1586e5d1b482a8d
Aziz <aziz.khadjayev070@gmail.com>
2022-10-29 10:27:11 +0200

    APP-04 Import List items

--- src/components/Home/Search/SearchLists.tsx
+++ src/components/Home/Search/SearchLists.tsx
@@ -1,5 +1,15 @@
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+
 const SearchLists = () => {
-  return <div>SearchLists</div>;
+  return (
+    <List>
+      <ListItem>
+        <ListItemText primary="Inbox" />
+      </ListItem>
+    </List>
+  );
 };
 
 export default SearchLists;
