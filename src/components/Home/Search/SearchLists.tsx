9d295753a275df099683bd60937a2493b90116f3
Aziz <aziz.khadjayev070@gmail.com>
2022-10-30 14:23:47 +0100

    APP-04 Remove array

--- src/components/Home/Search/SearchLists.tsx
+++ src/components/Home/Search/SearchLists.tsx
@@ -1,16 +1,5 @@
 import { ListItemButton, List, ListItem, ListItemText } from "@mui/material";
 
-// Refactor list and import above
-const searchTypes: string[] = [
-  "Classification",
-  "Work Type",
-  "Period",
-  "Place",
-  "Century",
-  "Gallery",
-  "On View",
-];
-
 const SearchLists = () => {
   return (
     <List sx={{ display: "flex" }}>
