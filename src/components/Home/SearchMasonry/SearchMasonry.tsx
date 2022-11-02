210af65554c61d6449c5fc1dc4b8de03b9374a44
Aziz <aziz.khadjayev070@gmail.com>
2022-11-02 15:43:29 +0100

    APP-04 Prepare for masoanty

--- src/components/Home/SearchMasonry/SearchMasonry.tsx
+++ src/components/Home/SearchMasonry/SearchMasonry.tsx
@@ -1,7 +1,11 @@
 import { Container } from "@mui/material";
+import Box from "@mui/material/Box";
+import { styled } from "@mui/material/styles";
+import Paper from "@mui/material/Paper";
+import Masonry from "@mui/lab/Masonry";
 
 const SearchMasonry = () => {
-  return <Container>SearchMasonry</Container>;
+  return <Container></Container>;
 };
 
 export default SearchMasonry;
