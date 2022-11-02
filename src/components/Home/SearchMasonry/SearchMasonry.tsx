add3ed8df1aff46d0d70508346f14a90521c2595
Aziz <aziz.khadjayev070@gmail.com>
2022-11-02 15:44:33 +0100

    APP-04 Use masonary comp

--- src/components/Home/SearchMasonry/SearchMasonry.tsx
+++ src/components/Home/SearchMasonry/SearchMasonry.tsx
@@ -4,8 +4,18 @@ import { styled } from "@mui/material/styles";
 import Paper from "@mui/material/Paper";
 import Masonry from "@mui/lab/Masonry";
 
+const heights = [
+  150, 30, 90, 70, 110, 150, 130, 80, 50, 90, 100, 150, 30, 50, 80,
+];
+
 const SearchMasonry = () => {
-  return <Container></Container>;
+  return (
+    <Container>
+      <Box sx={{ width: 500, minHeight: 393 }}>
+        <Masonry columns={4} spacing={2}></Masonry>
+      </Box>
+    </Container>
+  );
 };
 
 export default SearchMasonry;
