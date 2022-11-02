b4d2e792f19cf1918a2798be54827e3b9b546d61
Aziz <aziz.khadjayev070@gmail.com>
2022-11-02 15:44:59 +0100

    APP-04 Loop throut the masonary

--- src/components/Home/SearchMasonry/SearchMasonry.tsx
+++ src/components/Home/SearchMasonry/SearchMasonry.tsx
@@ -12,7 +12,13 @@ const SearchMasonry = () => {
   return (
     <Container>
       <Box sx={{ width: 500, minHeight: 393 }}>
-        <Masonry columns={4} spacing={2}></Masonry>
+        <Masonry columns={4} spacing={2}>
+          {heights.map((height, index) => (
+            <Item key={index} sx={{ height }}>
+              {index + 1}
+            </Item>
+          ))}
+        </Masonry>
       </Box>
     </Container>
   );
