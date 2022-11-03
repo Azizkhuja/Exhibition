37a24d09f70ba7115492afbce350106887632d0d
Aziz <aziz.khadjayev070@gmail.com>
2022-11-03 12:57:35 +0100

    APP-04 Improve import mui

--- src/components/Home/SearchMasonry/SearchMasonry.tsx
+++ src/components/Home/SearchMasonry/SearchMasonry.tsx
@@ -1,7 +1,5 @@
-import { Container } from "@mui/material";
-import Box from "@mui/material/Box";
 import { styled } from "@mui/material/styles";
-import Paper from "@mui/material/Paper";
+import { Container, Paper, Box } from "@mui/material";
 import Masonry from "@mui/lab/Masonry";
 
 const heights = [
