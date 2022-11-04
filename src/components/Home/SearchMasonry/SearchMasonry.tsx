a7267e9895883509ab2935e3854501d6ff14bacb
Aziz <aziz.khadjayev070@gmail.com>
2022-11-04 17:06:32 +0100

    APP-04 Change media query breakpoint

--- src/components/Home/SearchMasonry/SearchMasonry.tsx
+++ src/components/Home/SearchMasonry/SearchMasonry.tsx
@@ -8,6 +8,7 @@ const imgArray = [
   "https://images.pexels.com/photos/161154/stained-glass-spiral-circle-pattern-161154.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
   "https://images.pexels.com/photos/962312/pexels-photo-962312.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
   "https://images.pexels.com/photos/1193743/pexels-photo-1193743.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  "https://images.pexels.com/photos/1646953/pexels-photo-1646953.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
   "https://images.pexels.com/photos/3246665/pexels-photo-3246665.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
   "https://images.pexels.com/photos/161154/stained-glass-spiral-circle-pattern-161154.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
   "https://images.pexels.com/photos/962312/pexels-photo-962312.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
@@ -16,6 +17,7 @@ const imgArray = [
   "https://images.pexels.com/photos/161154/stained-glass-spiral-circle-pattern-161154.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
   "https://images.pexels.com/photos/962312/pexels-photo-962312.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
   "https://images.pexels.com/photos/1193743/pexels-photo-1193743.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  "https://images.pexels.com/photos/1646953/pexels-photo-1646953.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
 ];
 
 const Item = styled(Paper)(({ theme }) => ({
@@ -28,7 +30,7 @@ const Item = styled(Paper)(({ theme }) => ({
 
 const SearchMasonry = () => {
   const belowMdSize = useMediaQuery("(max-width:900px)");
-  const belowXsSize = useMediaQuery("(max-width:400px)");
+  const belowXsSize = useMediaQuery("(max-width:500px)");
 
   return (
     <Container>
