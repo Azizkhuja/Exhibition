3ff61715d34964c490cabe67be8bea86d65d4fb4
Aziz <aziz.khadjayev070@gmail.com>
2022-11-11 15:35:03 +0100

    APP-04 Nested grid

--- src/components/Home/Footer/Footer.tsx
+++ src/components/Home/Footer/Footer.tsx
@@ -6,6 +6,10 @@ const Footer = () => {
       <Grid container spacing={2}>
         <Grid item md={4}>
           1
+          <Grid container sx={{ flexDirection: "column" }}>
+            <Grid item></Grid>
+            <Grid item>2</Grid>
+          </Grid>
         </Grid>
         <Grid item md={4}>
           2
