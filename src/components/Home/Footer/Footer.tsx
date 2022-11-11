831a57bb1e27dd42e830d816fdb3d2f54abbb3fb
Aziz <aziz.khadjayev070@gmail.com>
2022-11-11 15:32:26 +0100

    APP-04 Use grid in footer

--- src/components/Home/Footer/Footer.tsx
+++ src/components/Home/Footer/Footer.tsx
@@ -1,7 +1,21 @@
-import { Container } from "@mui/material";
+import { Container, Grid } from "@mui/material";
 
 const Footer = () => {
-  return <Container></Container>;
+  return (
+    <Container>
+      <Grid container spacing={2}>
+        <Grid item md={4}>
+          1
+        </Grid>
+        <Grid item md={4}>
+          2
+        </Grid>
+        <Grid item md={4}>
+          3
+        </Grid>
+      </Grid>
+    </Container>
+  );
 };
 
 export default Footer;
