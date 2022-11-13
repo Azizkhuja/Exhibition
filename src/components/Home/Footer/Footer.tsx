e44c701e5cfa45e7b9f2a51637e82ddc0014358e
Aziz <aziz.khadjayev070@gmail.com>
2022-11-13 13:07:40 +0100

    APP-04 use icons

--- src/components/Home/Footer/Footer.tsx
+++ src/components/Home/Footer/Footer.tsx
@@ -11,6 +11,10 @@ import {
 
 // Icons
 import BeachAccessSharpIcon from "@mui/icons-material/BeachAccessSharp";
+import LanguageIcon from "@mui/icons-material/Language";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 const Footer = () => {
   return (
