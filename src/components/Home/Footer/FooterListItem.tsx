5985b6b941467f1c06845295ab72a1f63235446a
Aziz <aziz.khadjayev070@gmail.com>
2022-11-19 08:52:02 +0100

    APP-04 Loop through the rules data

--- src/components/Home/Footer/FooterListItem.tsx
+++ src/components/Home/Footer/FooterListItem.tsx
@@ -1,10 +1,27 @@
+import { styled } from "@mui/material/styles";
+import { List, ListItem, ListItemButton } from "@mui/material";
+
 type DataTypes = {
-  policies: string[];
-  services: string[];
+  policies?: string[];
+  services?: string[];
 };
 
 const FooterListItem = ({ policies, services }: DataTypes) => {
-  return <div>FooterListItem</div>;
+  return (
+    <List>
+      {policies?.map((item: string) => {
+        return (
+          <ListItem disablePadding>
+            <NewsletterItem>{item}</NewsletterItem>
+          </ListItem>
+        );
+      })}
+    </List>
+  );
 };
+
+const NewsletterItem = styled(ListItemButton)`
+  padding: 1px;
+`;
 
 export default FooterListItem;
