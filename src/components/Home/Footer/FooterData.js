3f2371fffde6de543de788ef6eb218a43ba5607e
Aziz <aziz.khadjayev070@gmail.com>
2022-11-20 10:24:05 +0100

    APP-04 Add data to file

---
+++ src/components/Home/Footer/FooterData.js
@@ -0,0 +1,19 @@
+// Data
+export const policies = [
+  "Terms of Use",
+  "Privacy Statement",
+  "Digital Accessibility",
+  "FAQs",
+];
+export const services = [
+  "Special Event Rentals",
+  "Comment Card",
+  "Image Licensing",
+  "API",
+];
+export const addressInfo = [
+  "Harvard Art Museums",
+  "32 Quincy Street",
+  "Cambridge, MA 02138",
+  "1 (617) 495-9400",
+];
