c8d1e7137d556e3b4cb97f956b604c114c7b1457
Aziz <aziz.khadjayev070@gmail.com>
2022-11-30 07:49:36 +0100

    APP-04 Remove return in map

--- src/components/Home/Footer/FooterListItem.tsx
+++ src/components/Home/Footer/FooterListItem.tsx
@@ -11,20 +11,16 @@ const FooterListItem = ({ policies, services }: DataTypes) => {
   const belowMdSize = useMediaQuery("(max-width:900px)");
   return (
     <List sx={{ fontSize: belowMdSize ? 16 : 12 }}>
-      {policies?.map((item: string) => {
-        return (
-          <ListItem disablePadding>
-            <NewsletterItem>{item}</NewsletterItem>
-          </ListItem>
-        );
-      })}
-      {services?.map((item: string) => {
-        return (
-          <ListItem disablePadding>
-            <NewsletterItem>{item}</NewsletterItem>
-          </ListItem>
-        );
-      })}
+      {policies?.map((item: string) => (
+        <ListItem disablePadding>
+          <NewsletterItem>{item}</NewsletterItem>
+        </ListItem>
+      ))}
+      {services?.map((item: string) => (
+        <ListItem disablePadding>
+          <NewsletterItem>{item}</NewsletterItem>
+        </ListItem>
+      ))}
     </List>
   );
 };
