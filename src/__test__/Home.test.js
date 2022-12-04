10b009ec4503a821864f25e446fbfe25e59fce08
Aziz <aziz.khadjayev070@gmail.com>
2022-12-04 21:30:42 +0100

    APP-04 Create home test file

--- src/__test__/Home.test.js
+++ src/__test__/Home.test.js
@@ -1,2 +1,9 @@
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
+import Search from "../components/Home/Search/Search";
+
+describe("Test", () => {
+  it("should get navbar title", () => {
+    render(<Search />);
+  });
+});
