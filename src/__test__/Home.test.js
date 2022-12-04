657252a7099fbae360ac2b53176b9b2d0d367819
Aziz <aziz.khadjayev070@gmail.com>
2022-12-04 21:32:40 +0100

    APP-04 Get search comonent title

--- src/__test__/Home.test.js
+++ src/__test__/Home.test.js
@@ -3,7 +3,7 @@ import "@testing-library/jest-dom";
 import Search from "../components/Home/Search/Search";
 
 describe("Should get home page and nested components", () => {
-  it("should get navbar title", () => {
+  it("should get subtitle of search component", () => {
     render(<Search />);
     const getTitle = screen.getByText(/Fogg/i);
     expect(getTitle).toBeInTheDocument();
