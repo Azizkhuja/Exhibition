a7438686683a827302612c788617044782e5a386
Aziz <aziz.khadjayev070@gmail.com>
2022-12-04 21:34:06 +0100

    APP-04 Test get whole subitle

--- src/__test__/Home.test.js
+++ src/__test__/Home.test.js
@@ -3,7 +3,7 @@ import "@testing-library/jest-dom";
 import Search from "../components/Home/Search/Search";
 
 describe("Should get home page and nested components", () => {
-  it("should get subtitle of search component", () => {
+  it("should get whole subtitle of search component", () => {
     render(<Search />);
     const getTitle = screen.getByText(/The Harvard Art Museums/i);
     expect(getTitle).toBeInTheDocument();
