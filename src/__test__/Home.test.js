a4f6ca61cda5cb90ae58a4a21f89448e99970273
Aziz <aziz.khadjayev070@gmail.com>
2022-12-04 21:34:54 +0100

    APP-04 Add other getters

--- src/__test__/Home.test.js
+++ src/__test__/Home.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, getByTestId, getByRole } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Search from "../components/Home/Search/Search";
 
