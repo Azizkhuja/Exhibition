808adeb2a52d2e26d2fc5212223ed1e76a8d8743
Aziz <aziz.khadjayev070@gmail.com>
2022-12-04 21:33:44 +0100

    APP-04 Update title of thet

--- src/__test__/Home.test.js
+++ src/__test__/Home.test.js
@@ -5,7 +5,7 @@ import Search from "../components/Home/Search/Search";
 describe("Should get home page and nested components", () => {
   it("should get subtitle of search component", () => {
     render(<Search />);
-    const getTitle = screen.getByText(/Fogg/i);
+    const getTitle = screen.getByText(/The Harvard Art Museums/i);
     expect(getTitle).toBeInTheDocument();
   });
 });
