5e888a7189b3316aa32af5a42b4bb2f2e64be16b
Aziz <aziz.khadjayev070@gmail.com>
2022-12-05 07:33:47 +0100

    APP-04 Add test main

--- src/__test__/Home.test.js
+++ src/__test__/Home.test.js
@@ -1,8 +1,12 @@
 import { render, screen, getByTestId, getByRole } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Search from "../components/Home/Search/Search";
+import Main from "../components/Home/Main/Main";
 
 describe("Should get home page and nested components", () => {
+  it("should get whole subtitle of search component", () => {
+    render(<Main />);
+  });
   it("should get whole subtitle of search component", () => {
     render(<Search />);
     const getTitle = screen.getByText(/The Harvard Art Museums/i);
