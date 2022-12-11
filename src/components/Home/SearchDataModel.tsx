da72cc3fc4b4d694fe0ff84fbbf60516b21a9a77
Aziz <aziz.khadjayev070@gmail.com>
2022-12-11 12:05:53 +0100

    APP-04 Create Mainpage type

--- src/components/Home/SearchDataModel.tsx
+++ src/components/Home/SearchDataModel.tsx
@@ -1,3 +1,68 @@
+export interface MainPageData {
+  copyright: null;
+  contextualtextcount: number;
+  creditline: string;
+  accesslevel: number;
+  dateoflastpageview: Date;
+  classificationid: number;
+  division: Division;
+  markscount: number;
+  publicationcount: number;
+  totaluniquepageviews: number;
+  contact: Contact;
+  colorcount: number;
+  rank: number;
+  details: Details;
+  state: null;
+  id: number;
+  verificationleveldescription: string;
+  period: null | string;
+  images: Image[];
+  worktypes: WorktypeElement[];
+  imagecount: number;
+  totalpageviews: number;
+  accessionyear: number;
+  standardreferencenumber: string;
+  signed: null;
+  classification: Classification;
+  relatedcount: number;
+  verificationlevel: number;
+  primaryimageurl: string;
+  titlescount: number;
+  peoplecount: number;
+  style: null;
+  lastupdate: Lastupdate;
+  commentary: null;
+  periodid: number | null;
+  technique: Technique;
+  edition: null;
+  description: null;
+  medium: Medium;
+  lendingpermissionlevel: number;
+  title: string;
+  accessionmethod: Accessionmethod;
+  colors: Color[];
+  provenance: null;
+  groupcount: number;
+  dated: string;
+  department: Department;
+  dateend: number;
+  url: string;
+  dateoffirstpageview: Date;
+  century: null;
+  objectnumber: string;
+  labeltext: null;
+  datebegin: number;
+  culture: Culture;
+  exhibitioncount: number;
+  imagepermissionlevel: number;
+  mediacount: number;
+  objectid: number;
+  techniqueid: number;
+  dimensions: string;
+  seeAlso: SeeAlso[];
+}
+
 export enum Accessionmethod {
   Bequest = "Bequest",
 }
