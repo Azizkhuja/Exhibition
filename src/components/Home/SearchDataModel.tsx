e396421e413b57224751a5edc8f2a12ea239771c
Aziz <aziz.khadjayev070@gmail.com>
2022-12-11 12:03:58 +0100

    APP-04 Image tyoe

--- src/components/Home/SearchDataModel.tsx
+++ src/components/Home/SearchDataModel.tsx
@@ -1,3 +1,29 @@
+export interface Image {
+  date: Date;
+  copyright: Copyright;
+  imageid: number;
+  idsid: number;
+  format: ImageFormat;
+  description: null;
+  technique: null;
+  renditionnumber: string;
+  displayorder: number;
+  baseimageurl: string;
+  alttext: null;
+  width: number;
+  publiccaption: null;
+  iiifbaseuri: string;
+  height: number;
+}
+
+export enum Copyright {
+  PresidentAndFellowsOfHarvardCollege = "President and Fellows of Harvard College",
+}
+
+export enum ImageFormat {
+  ImageJPEG = "image/jpeg",
+}
+
 export enum Lastupdate {
   The20221210T0529350500 = "2022-12-10T05:29:35-0500",
   The20221210T0529360500 = "2022-12-10T05:29:36-0500",
