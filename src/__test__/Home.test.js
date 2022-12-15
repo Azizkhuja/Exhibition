2a40075a1e0b5ee706440c777da963c923e03956
Aziz <aziz.khadjayev070@gmail.com>
2022-12-15 07:03:45 +0100

    APP-04 Get period button from search

--- src/__test__/Home.test.js
+++ src/__test__/Home.test.js
@@ -39,7 +39,7 @@ describe("Should get home page and nested components", () => {
   });
   it("get SearchLists component button: work type and should be in the documents", () => {
     render(<SearchLists />);
-    const searchInput = screen.getByText(/work type/i);
+    const searchInput = screen.getByText(/period/i);
     expect(searchInput).toBeInTheDocument();
   });
 });
