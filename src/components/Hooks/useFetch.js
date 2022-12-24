a74b222cb9c76168bcc42ab840455932ed8895df
Aziz <aziz.khadjayev070@gmail.com>
2022-12-24 16:34:20 +0100

    APP-04 Fix useFetch

--- src/components/Hooks/useFetch.js
+++ src/components/Hooks/useFetch.js
@@ -18,7 +18,7 @@ export default function useFetch(url) {
         setLoading(false);
       }
     })();
-
-    return { data, error, loading };
   }, [url]);
+
+  return { data, error, loading };
 }
