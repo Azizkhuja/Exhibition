02c119f6c64d261784314e85378959b6f01b0c2a
Aziz <aziz.khadjayev070@gmail.com>
2022-12-24 10:04:39 +0100

    APP-04 Create init state

--- src/components/Hooks/useFetch.js
+++ src/components/Hooks/useFetch.js
@@ -1,4 +1,8 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-export function useFetch() {}
+export default function useFetch(url) {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+}
