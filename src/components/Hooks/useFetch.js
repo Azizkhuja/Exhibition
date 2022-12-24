b1d5bf0e22ca7e1a69b9f6a7aeb43b1cf6ba71fa
Aziz <aziz.khadjayev070@gmail.com>
2022-12-24 10:05:31 +0100

    APP-04 Create async function

--- src/components/Hooks/useFetch.js
+++ src/components/Hooks/useFetch.js
@@ -5,4 +5,18 @@ export default function useFetch(url) {
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    (async function () {
+      try {
+        setLoading(true);
+        const response = await axios.get(url);
+        setData(response.data);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    })();
+  }, [url]);
 }
