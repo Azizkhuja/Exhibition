58e192d7140d9c510b10cb3f65bb6d97587e8a0e
Aziz <aziz.khadjayev070@gmail.com>
2022-12-26 07:52:29 +0100

    APP-04 Import box and progress bar

--- src/components/Home/Main/Loading.tsx
+++ src/components/Home/Main/Loading.tsx
@@ -1,1 +1,6 @@
-export default function Loading() {}
+import Box from "@mui/material/Box";
+import LinearProgress from "@mui/material/LinearProgress";
+
+export default function Loading() {
+  return <Box sx={{ width: "100%" }}></Box>;
+}
