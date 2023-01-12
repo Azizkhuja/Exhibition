345205fb1f80c2e7e6cb74b7bd9a717ad68ff67c
Aziz <aziz.khadjayev070@gmail.com>
2023-01-12 07:21:37 +0100

    APP-04 Test desc

--- src/__test__/Home.test.js
+++ src/__test__/Home.test.js
@@ -80,5 +80,5 @@ describe("Should get home page and nested components", () => {
     expect(getFooterLogo).toBeInTheDocument();
   });
   // Get "Meno About" button in Home page footer
-  it("", () => {});
+  it("get about menu in footer", () => {});
 });
