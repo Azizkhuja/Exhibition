ff6bd63dcf2126892223495984468f61532ea213
Aziz <aziz.khadjayev070@gmail.com>
2023-01-13 07:58:40 +0100

    APP-04 Update comment

--- src/__test__/Home.test.js
+++ src/__test__/Home.test.js
@@ -79,7 +79,7 @@ describe("Should get home page and nested components", () => {
     });
     expect(getFooterLogo).toBeInTheDocument();
   });
-  // Get "Meno About" button in Home page footer
+  // Get "Menu About" button in Home page footer
   it("get about menu in footer", () => {
     render(<Footer />);
     const getAboutMenu = screen.getByRole("button", { name: /about/i });
