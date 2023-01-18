8218c1d0238364ca8ee4e551714878a4f895baef
Aziz <aziz.khadjayev070@gmail.com>
2023-01-18 07:12:42 +0100

    Update variable name

--- src/__test__/Home.test.js
+++ src/__test__/Home.test.js
@@ -94,7 +94,7 @@ describe("Should get home page and nested components", () => {
   // Get "Contact US" button in Home page footer
   it("get contact us in footer", () => {
     render(<Footer />);
-    const getContactUsg = screen.getByRole("button", { name: /contact us/i });
-    expect(getContactUsg).toBeInTheDocument();
+    const getContactUs = screen.getByRole("button", { name: /contact us/i });
+    expect(getContactUs).toBeInTheDocument();
   });
 });
