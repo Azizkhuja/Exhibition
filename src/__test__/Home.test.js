86a691eb4b8d91bbd2dfe3c8971188f9c4631367
Aziz <aziz.khadjayev070@gmail.com>
2023-01-29 17:55:27 +0100

    APP-04 Add description for Empoyment

--- src/__test__/Home.test.js
+++ src/__test__/Home.test.js
@@ -98,5 +98,5 @@ describe("Should get home page and nested components", () => {
     expect(getContactUs).toBeInTheDocument();
   });
   // Get "Employment" button in Home page footer
-  it("", () => {});
+  it("get Employment in footer", () => {});
 });
