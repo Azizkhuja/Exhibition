7beaf6e7ffb8ffef0e7280eea768f3d0652a6102
Aziz <aziz.khadjayev070@gmail.com>
2023-02-17 22:15:00 +0100

    APP-04 Get privicy

--- src/__test__/Home.test.js
+++ src/__test__/Home.test.js
@@ -157,5 +157,8 @@ describe("Should get home page and nested components", () => {
   // Get "Privacy statement" text button in Home page footer
   it("Get 'Privacy statement' button in Home page footer", () => {
     render(<Footer />);
+    screen.getByRole("button", {
+      name: /privacy statement/i,
+    });
   });
 });
