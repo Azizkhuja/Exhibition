0cc8630c93bb14feb0d2b03356d79596a04e33f9
Aziz <aziz.khadjayev070@gmail.com>
2023-02-25 08:55:09 +0100

    APP-04 Add test case description

--- src/__test__/Home.test.js
+++ src/__test__/Home.test.js
@@ -178,5 +178,5 @@ describe("Should get home page and nested components", () => {
     });
   });
   // Get "Address" in Home page footer
-  it("", () => {});
+  it("Get 'Special Event Rentals' in Home page footer", () => {});
 });
