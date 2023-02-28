61405054535ef0988e43f5af7fa2d9c3afb28217
Aziz <aziz.khadjayev070@gmail.com>
2023-02-28 17:33:30 +0500

    APP-04 Add test desc

--- src/__test__/Home.test.js
+++ src/__test__/Home.test.js
@@ -187,5 +187,5 @@ describe("Should get home page and nested components", () => {
     expect(getEventsText).toBeInTheDocument();
   });
   // Get "Comment card" in Home page footer
-  it("", () => {});
+  it("Get 'Comment card' in Home page footer", () => {});
 });
