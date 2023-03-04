1fb3adf01c4164854f450c603e2c54e4dfe02a5a
Aziz <aziz.khadjayev070@gmail.com>
2023-03-04 15:01:12 +0500

    APP-04 Add test case

--- src/__test__/Home.test.js
+++ src/__test__/Home.test.js
@@ -202,5 +202,5 @@ describe("Should get home page and nested components", () => {
     });
     expect(getImageLicensingText).toBeInTheDocument();
   });
-  // Get "Image licensing" in Home page footer
+  // Get "API" in Home page footer
 });
