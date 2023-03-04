c34a97353b913acbf44c3dcc9f11175eb5780484
Aziz <aziz.khadjayev070@gmail.com>
2023-03-04 14:59:42 +0500

    APP-04 Store as a variable

--- src/__test__/Home.test.js
+++ src/__test__/Home.test.js
@@ -197,7 +197,7 @@ describe("Should get home page and nested components", () => {
   // Get "Image licensing" in Home page footer
   it("Get 'Image licensing' in Home page footer", () => {
     render(<Footer />);
-    screen.getByRole("button", {
+    const getImageLicensingText = screen.getByRole("button", {
       name: /image licensing/i,
     });
   });
