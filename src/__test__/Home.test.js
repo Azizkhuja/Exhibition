26e94b58fee88d3c57ac680ae24b02aaa8875bf1
Aziz <aziz.khadjayev070@gmail.com>
2023-03-06 11:01:55 +0500

    APP-04 Get API and store as variable

--- src/__test__/Home.test.js
+++ src/__test__/Home.test.js
@@ -205,7 +205,7 @@ describe("Should get home page and nested components", () => {
   // Get "API" in Home page footer
   it("Get 'API' in Home page footer", () => {
     render(<Footer />);
-    const getImageLicensingText = screen.getByRole("button", {
+    const getAPIText = screen.getByRole("button", {
       name: /api/i,
     });
   });
