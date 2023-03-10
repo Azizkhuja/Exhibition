b418aa79d9a546b01f29a76cce2e5cc52c0d8a39
Aziz <aziz.khadjayev070@gmail.com>
2023-03-10 13:53:51 +0500

    APP-04 Create footer variable and assign component

--- src/__test__/Home.test.js
+++ src/__test__/Home.test.js
@@ -9,7 +9,7 @@ import { Typography } from "@mui/material";
 const searchComponent = <Search />;
 const searchListsComponent = <SearchLists />;
 const searchMasonryComponent = <SearchMasonry />;
-const footerComponent
+const footerComponent = <Footer />;
 
 describe("Should get home page and nested components", () => {
   // Get subtitle of the application in the main page
