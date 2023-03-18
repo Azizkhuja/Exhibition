8aec1df0b9b742c23a244be1e8510e784903a122
Aziz <aziz.khadjayev070@gmail.com>
2023-03-18 19:00:05 +0500

    APP-04 Use footerComponent variable in Sign up to get the latest news

--- src/__test__/Home.test.js
+++ src/__test__/Home.test.js
@@ -147,7 +147,7 @@ describe("Should get home page and nested components", () => {
   });
   // Get "Sign up to get the latest news" text in Home page footer
   it("Get 'Sign up to get the latest news' button in Home page footer", () => {
-    render(<Footer />);
+    render(footerComponent);
     const getSignUpText = screen.getByText(/sign up to get the latest news/i);
     expect(getSignUpText).toBeInTheDocument();
   });
